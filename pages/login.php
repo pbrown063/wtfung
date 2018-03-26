0154cdabaa7a37e0ba7116f9d7ba33e103049ef9
Parker Brown <pbrown063@gmail.com>
2018-03-25 22:01:33 -0700

    Still figuring out styles.

--- pages/login.php
+++ pages/login.php
@@ -3,8 +3,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WTF Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>WTF Login</title>
   <link rel="stylesheet" type="text/css" href="CSS/form_style.css">
   <link rel="stylesheet" type="text/css" href="CSS/main_style.css">
 <body>
@@ -13,18 +13,18 @@
 </nav>
 <div class="container">
   <form method="POST" action="authenticate.php">
-        <ul class="flex-outer">
-            <li>
-                <input type="email" id="email" placeholder="Email Address" name="email" required>
-            </li>
-            <li>
-                <input type="password" id="password" placeholder="Password" name="password" required>
-            </li>
-            <li>
-                <button type="submit"> Log in</button>
-            </li>
-        </ul>
-    </form>
+    <ul class="flex-outer">
+      <li>
+        <input type="email" id="email" placeholder="Email Address" name="email" required>
+      </li>
+      <li>
+        <input type="password" id="password" placeholder="Password" name="password" required>
+      </li>
+      <li>
+        <button type="submit"> Log in</button>
+      </li>
+    </ul>
+  </form>
 </div>
 </body>
 <footer>
