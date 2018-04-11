be86a694259bf01d74de141fcb3021ba1dbbba76
Khorne <kaylan.horne3@gmail.com>
2018-04-11 23:27:03 +0000

    Added ctype_space and addslashes funtction to substrate.php

--- pages/substrate.php
+++ pages/substrate.php
@@ -2,7 +2,7 @@
 //file containing functions -- needed for every page
  require_once __DIR__ . '/bootstrap.php';
 
-if (!filter_input(INPUT_POST, "submit")) {
+if (!filter_input(INPUT_POST, "submit")  || ctype_space(filter_input(INPUT_POST, "substrate"))) {
 
 header('Location: substrate_form.php');
 die();
@@ -10,8 +10,8 @@ die();
 } else {
 
 //grabbing inputs from posted form and making variables
-  $substrate = filter_input(INPUT_POST, "substrate");
-  $notes = filter_input(INPUT_POST, "notes");
+  $substrate = addslashes(filter_input(INPUT_POST, "substrate"));
+  $notes = addslashes(filter_input(INPUT_POST, "notes"));
 //query for insertion to database
   $insert = "INSERT INTO substrate_library (substrate_type, notes) VALUES (lower('$substrate'), lower('$notes'));";
 //connect to database
