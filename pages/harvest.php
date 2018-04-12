647fad8713d7f20e547013af0d424fa08972815b
Khorne <kaylan.horne3@gmail.com>
2018-04-12 23:16:32 +0000

    Added functions (ctype_space, addslashes) to harvest page. tested. works.

--- pages/harvest.php
+++ pages/harvest.php
@@ -4,19 +4,20 @@ require_once __DIR__ . '/bootstrap.php';
 
 
 //check for if they hit submit button or first time landing on the page
-if (!filter_input(INPUT_POST, "submit")) {
+if (!filter_input(INPUT_POST, "submit") || ctype_space(filter_input(INPUT_POST,"batch")) || ctype_space(filter_input(INPUT_POST,"strain")) ||
+    ctype_space(filter_input(INPUT_POST,"weight"))) {
 
 header('Location: harvest_form.php');
 die();
 
 } else {
 //grabbing inputs from posted form and making variables
-$batch = filter_input(INPUT_POST, "batch");
-$strain = filter_input(INPUT_POST, "strain");
-$date = filter_input(INPUT_POST, "date");
-$weight = filter_input(INPUT_POST, "weight");
-$time = filter_input(INPUT_POST, "time");
-$notes = filter_input(INPUT_POST, "notes");
+$batch = addslashes(filter_input(INPUT_POST, "batch"));
+$strain = addslashes(filter_input(INPUT_POST, "strain"));
+$date = addslashes(filter_input(INPUT_POST, "date"));
+$weight = addslashes(filter_input(INPUT_POST, "weight"));
+$time = addslashes(filter_input(INPUT_POST, "time"));
+$notes = addslashes(filter_input(INPUT_POST, "notes"));
 //query for insertion to database
 $insert = "INSERT INTO harvest (date, weight, time, notes, batch, strain) VALUES ('$date', '$weight', '$time', '$notes','$batch', '$strain');";
 //connect to database
