284c7bc8c57a21ea1c10323b0f0208f274955a9a
sheldon lal <sheldon.lal@myokanagan.bc.ca>
2018-04-15 20:18:07 +0000

    WIP...cookies in authenticate are being checked with a function in common.inc called is brian, it is trapping in authenticate.php

--- pages/authenticate.php
+++ pages/authenticate.php
@@ -1,7 +1,5 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
-$cookie_auth_value = 1;
-$cookie_not_value = 0;
 
 if (isset($_POST['email']) && isset($_POST['password'])){
   $mysqli = sql_connect();
@@ -12,6 +10,8 @@ if (isset($_POST['email']) && isset($_POST['password'])){
     "' AND password = '".$userpasswd."'";
 
   $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+  
+  setcookie("Authorized", 0)
 
   if (mysqli_num_rows($result) >= 1) {
     while ($info = mysqli_fetch_array($result)) {
@@ -20,14 +20,12 @@ if (isset($_POST['email']) && isset($_POST['password'])){
       $l_name = stripslashes($info['lastname']);
       $_SESSION['name'] = $f_name . ' ' . $l_name;
       $cookie_email = $useremail;
-      setcookie($cookie_email, $cookie_auth_value);
-      header('Location: home.php');
-
-    }
+     setcookie("email", $cookie_email);
+     is_brian();    
+}
   }
   else {
-    setcookie($cookie_email, $cookie_not_value);
-    header('Location: login.php');
+      header('Location: login.php');
   }
   mysqli_close($mysqli);
 }
