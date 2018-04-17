7405cb699e113a9ba580cddd91d978d88a2c1512
Parker Brown <pbrown@acromedia.com>
2018-04-16 18:44:26 -0700

    Added data-analytics style page wrappers.

--- pages/lineandshade-1.php
+++ pages/lineandshade-1.php
@@ -1,3 +1,28 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>What The Fungus</title>
+  <link rel="stylesheet" type="text/css" href="CSS/form_style.css">
+  <link rel="stylesheet" type="text/css" href="CSS/main_style.css">
+</head>
+<body>
+<?php require_once 'header.php'; ?>
+<div class="data_button">
+  <a href="3dpie-1.php">3d PIE CHART</a>
+</div>
+<div class="data_button">
+  <a href="balloon-1.php">BALLON CHART</a>
+</div>
+<div class="data_button">
+  <a href="lineandshade-1.php">LINE SHADED GRAPH</a>
+</div>
+
+<div class="data-graph">
 <?php // content="text/plain; charset=utf-8"
 require_once __DIR__ . '/../jpgraph/src/jpgraph.php';
 require_once __DIR__ . '/../jpgraph/src/jpgraph_line.php';
@@ -40,3 +65,10 @@ $graph->xaxis->title->SetFont(FF_FONT1,FS_BOLD);
 // Display the graph
 $graph->Stroke();
 ?>
+</div>
+
+</body>
+</html>
+
+
+
