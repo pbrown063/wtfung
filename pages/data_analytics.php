2480c1d2385b30e80bc14ba4614c33d92ce9c8f8
Parker Brown <pbrown@acromedia.com>
2018-04-16 19:19:41 -0700

    split graph display into files with button post

--- pages/data_analytics.php
+++ pages/data_analytics.php
@@ -1,5 +1,26 @@
 <?php
 require_once __DIR__ . '/bootstrap.php';
+require_once 'make-graph.php';
+require_once __DIR__ . '/../jpgraph/src/jpgraph.php';
+
+switch ($_POST_['click']) {
+
+  case '3dpie-graph':
+    $graph = '3dpie.php';
+    break;
+
+  case 'lineandshade-graph':
+    $graph = 'lineandshade.php';
+    break;
+
+  case 'balloon-graph':
+    $graph = 'balloon.php';
+    break;
+
+  default:
+    $graph = '3dpie.php';
+    break;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,18 +34,18 @@ require_once __DIR__ . '/bootstrap.php';
 <body>
 <?php require_once 'header.php'; ?>
 <form method="POST" action="make-graph.php">
-<div class="data_button">
-  <input type="submit" name="choose-graph" value="3dpie-graph"/>
-</div>
-<div class="data_button">
-  <input type="submit" name="choose-graph" value="lineandshade-graph"/>
-</div>
-<div class="data_button">
-  <input type="submit" name="choose-graph" value="balloon-graph"/>
-</div>
+  <div class="data_button">
+    <input type="submit" name="choose-graph" value="3dpie-graph"/>
+  </div>
+  <div class="data_button">
+    <input type="submit" name="choose-graph" value="lineandshade-graph"/>
+  </div>
+  <div class="data_button">
+    <input type="submit" name="choose-graph" value="balloon-graph"/>
+  </div>
 </form>
 <div class="graph-output">
-  <?php print '<img src="'.$graph-script.'"/>';?>
+  <?php print '<img src="'.$graph.'"/>';?>
 </div>
 </body>
 </html>
