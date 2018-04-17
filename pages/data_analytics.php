acf54571e8000427154e598c64ca71c4fbd94938
Parker Brown <pbrown@acromedia.com>
2018-04-16 20:51:11 -0700

    styling

--- pages/data_analytics.php
+++ pages/data_analytics.php
@@ -19,9 +19,9 @@ if (isset($_POST['graph'])) {
 <?php require_once 'header.php'; ?>
 
 <div class="container">
-  <h1>Select Graph to Display</h1>
   <form method="POST" action="data_analytics.php" id="graph-selector">
     <ul class="flex-outer">
+      <h1>Select Graph to Display</h1>
       <li>
         <button type="submit" name="graph" value="3dpie-graph">3D PIE GRAPH</button>
       </li>
@@ -31,16 +31,19 @@ if (isset($_POST['graph'])) {
       <li>
         <button type="submit" name="graph" value="balloon-graph">BALLOON GRAPH</button>
       </li>
+      <li>
+        <div class="graph-output">
+          <?php
+          if (isset($graph)) {
+            display_graph($graph);
+          }
+          ?>
+      </li>
     </ul>
   </form>
 </div>
 
-<div class="graph-output">
-  <?php
-  if (isset($graph)) {
-    display_graph($graph);
-  }
-  ?>
+
 </div>
 </body>
 </html>
