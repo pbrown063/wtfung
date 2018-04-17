335467537cc7f3132fc5917587aefd45ae3d592e
Parker Brown <pbrown@acromedia.com>
2018-04-16 18:22:49 -0700

    Populated Data Analytics page with examples.

---
+++ pages/data_analytics.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>What The Fungus</title>
+  <link rel="stylesheet" type="text/css" href="CSS/form_style.css">
+  <link rel="stylesheet" type="text/css" href="CSS/main_style.css">
+</head>
+<body>
+<?php require_once 'header.php'; ?>
+<div class="data_button">
+  <a href="3dpie-1.php">3d PIE CHART</a>
+</div>
+<div class="data_button">
+  <a href="balloon-1.php">BALLON CHART</a>
+</div>
+<div class="data_button">
+  <a href="lineandshade-1.php">LINE SHADED GRAPH</a>
+</div>
+</body>
+</html>
