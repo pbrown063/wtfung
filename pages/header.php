89e9059a5ba2b553cbc9300728562c7ccfc69996
Parker Brown <pbrown063@gmail.com>
2018-04-18 15:16:26 -0700

    protecting pages.

--- pages/header.php
+++ pages/header.php
@@ -12,10 +12,20 @@ if(is_admin()){
 }
 else if (is_farmer()) {
   $page = basename($_SERVER['PHP_SELF']);
+  $protected_page = 'data_analytics.php'
   $protected_pages = [
       'data_analytics.php',
       'strain_form.php',
+      'substrate_form.php',
+      'building_form.php',
+      'account_form.php',
+      'lineandshade.php',
+      'balloon.php',
+      '3dpie.php',
   ];
+  if ($page == $protected_page) {
+    header('Location: home.php');
+  }
   foreach($protected_pages as $value) {
     if ($page == $protected_pages) {
       header('Location: home.php');
