9662f8b040e87e4ce36daa884b583e84962361b6
Parker Brown <pbrown063@gmail.com>
2018-04-18 15:24:46 -0700

    protected pages arrayed.

--- pages/header.php
+++ pages/header.php
@@ -13,25 +13,22 @@ if(is_admin()){
 else if (is_farmer()) {
   $page = basename($_SERVER['PHP_SELF']);
   $protected_page = 'data_analytics.php';
-//  $protected_pages = [
-//      'data_analytics.php',
-//      'strain_form.php',
-//      'substrate_form.php',
-//      'building_form.php',
-//      'account_form.php',
-//      'lineandshade.php',
-//      'balloon.php',
-//      '3dpie.php',
-//  ];
+  $protected_pages = [
+      'data_analytics.php',
+      'strain_form.php',
+      'substrate_form.php',
+      'building_form.php',
+      'account_form.php',
+      'lineandshade.php',
+      'balloon.php',
+      '3dpie.php',
+  ];
 
-  if ($page == $protected_page) {
-    header('Location: home.php');
+  foreach($protected_pages as $value) {
+    if ($value == $protected_pages) {
+      header('Location: home.php');
+    }
   }
-//  foreach($protected_pages as $value) {
-//    if ($page == $protected_pages) {
-//      header('Location: home.php');
-//    }
-//  }
   echo get_header_menu($name);
 }
 else {
