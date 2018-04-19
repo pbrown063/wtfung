1f1bbafbd5988f89b7add3a892007c7175c71ca9
Parker Brown <pbrown@acromedia.com>
2018-04-18 19:01:21 -0700

    harvest greenhouse form and javascript working

--- pages/harvest_greenhouse_form.php
+++ pages/harvest_greenhouse_form.php
@@ -10,61 +10,86 @@ require_once __DIR__ . '/bootstrap.php';
 
   <link rel="stylesheet" type="text/css" href="./CSS/form_style.css">
   <link rel="stylesheet" type="text/css" href="./CSS/main_style.css">
+  <link rel="stylesheet" type="text/css" href="./CSS/table_style.css">
 
 </head>
 <body>
 <?php include 'header.php'; ?>
 <div class="container">
-  <form method="POST" action="harvest_greenhouse.php" id='harvest_greenhouse_form'>
-    <ul class="flex-outer">
-      <li>
-        <?php display_greehouse();?>
-      </li>
 
-      <!--
-      HERE IS THE START OF THE "ADD TO GREENHOUSE HARVEST SECTION
-      AIM IS TO BUILD AN ARRAY THAT WILL BE STEPPED THROUGH FOR INSERTS
-          -->
-      <li>
-        <input type='text' name='batch' placeholder='Batch Harvested'>
-      </li>
-      <li>
-        <?php display_strains();?>
-      </li>
-      <li>
-        <input type='text' name='weight' placeholder='Input weight'>
-      </li>
-      <li>
-        <input type='date' name='date' value="<?php current_date(); ?>" placeholder='yyyy-mm-d'> </input>
-      </li>
-      <li>
-        <ul class="flex-inner">
-          <li>
-            <label for="am">AM</label>
-            <input type='radio' name='time' value='am' id="am" checked>
-          </li>
-          <li>
-            <label for="pm">PM</label>
-            <input type='radio' name='time' value='pm' id="pm">
-          </li>
-        </ul>
-      </li>
-      <li>
-        <textarea name='notes' rows='5' cols='20' placeholder='Harvest Notes'></textarea>
-      </li>
-      <li>
 
-        <!--        THESE BUTTONS WILL DETERMINE IF MORE HARVESTS WILL BE MADE -->
-        <button type='submit' name='submit' value='submit' form='harvest_form'>Finished Harvesting</button>
-        <button onclick="add_batch_to_harvest_list()" value='add_batch'>Harvest Another Batch</button>
-      </li>
-    </ul>
-    <!--    THERE WILL BE A TABLE DISPLAY DOWN HERE THAT SHOWS ALL
-    OF THE BATCHES HARVESTED FOR THE GREENHOUSE IN QUEUE
-    SHOULD BE ABLE TO GENERATE A TABLE WITH THE JAVASCRIPT-->
+  <ul class="flex-outer">
+    <li>
+      <?php display_greehouse();?>
+    </li>
 
-  </form>
+    <!--
+    HERE IS THE START OF THE "ADD TO GREENHOUSE HARVEST SECTION
+    AIM IS TO BUILD AN ARRAY THAT WILL BE STEPPED THROUGH FOR INSERTS
+        -->
+    <form method="POST" action="harvest_greenhouse.php" id='harvest_greenhouse_form'>
+      <ul class="flex-outer">
+        <li>
+          <input type='text' name='batch' placeholder='Batch Harvested'>
+        </li>
+        <li>
+          <?php display_strains();?>
+        </li>
+        <li>
+          <input type='text' name='weight' placeholder='Input weight'>
+        </li>
+        <li>
+          <input type='date' name='date' value="<?php current_date(); ?>" placeholder='yyyy-mm-d'> </input>
+        </li>
+        <li>
+          <ul class="flex-inner">
+            <li>
+              <label for="am">AM</label>
+              <input type='radio' name='time' value='am' id="am" checked>
+            </li>
+            <li>
+              <label for="pm">PM</label>
+              <input type='radio' name='time' value='pm' id="pm">
+            </li>
+          </ul>
+        </li>
+        <li>
+          <textarea name='notes' rows='5' cols='20' placeholder='Harvest Notes'></textarea>
+        </li>
+        <li>
+
+          <!--        THESE BUTTONS WILL DETERMINE IF MORE HARVESTS WILL BE MADE -->
+          <button onclick="return add_batch_to_harvest_list()" value='add_batch'>Harvest Another Batch</button>
+        </li>
+      </ul>
+    </form>
+  </ul>
+
+
 </div>
+<div class="contain" id="harvest-queue-table">
+  <table>
+    <thead>
+    <tr>
+      <th>batch code</th>
+      <th>strain</th>
+      <th>weight</th>
+    </tr>
+    </thead>
+    <tbody id="harvest-queue">
+
+    </tbody>
+  </table>
+</div>
+
+<ul class="flex-outer">
+  <li>
+    <button type='submit' name='submit' value='submit' form='harvest_form'>Finished Harvesting</button>
+  </li>
+</ul>
+
+
+
 </body>
 <script src="harvest_greenhouse_script.js" type="text/javascript"></script>
 </html>
--- pages/harvest_greenhouse_script.js
+++ pages/harvest_greenhouse_script.js
@@ -1,14 +1,19 @@
 var harvest_list = [];
 
 function add_batch_to_harvest_list() {
+
+  document.getElementById("harvest_greenhouse_form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
   // Collect fields and push them onto the harvest_list for greenhouse.
-  var greenhouse = document.getElementsByName('greenhouse');
-  var batch_id = document.getElementsByName('batch');
-  var strain = document.getElementsByName('strain');
-  var weight = document.getElementsByName('weight');
-  var date = document.getElementsByName('date');
-  var time = document.getElementsByName('time');
-  var notes = document.getElementsByName('notes');
+  var greenhouse = document.getElementsByName('greenhouse')[0].value;
+  var batch_id = document.getElementsByName('batch')[0].value;
+  var strain = document.getElementsByName('strain')[0].value;
+  var weight = document.getElementsByName('weight')[0].value;
+  var date = document.getElementsByName('date')[0].value;
+  var time = document.getElementsByName('time')[0].value;
+  var notes = document.getElementsByName('notes')[0].value;
 
   var batch = {
     greenhouse: greenhouse,
@@ -22,27 +27,34 @@ function add_batch_to_harvest_list() {
 
   harvest_list.push(batch);
 
-  // Clear form and repopulate the greenhouse field value.
+  // Creates a new table row and outputs it on page
+  document.getElementById("harvest-queue-table").style.display = "block";
+  var body = document.getElementById("harvest-queue");
+  var harvest_row = document.createElement("TR");
+
+  var batch_row = document.createElement("TD");
+  var text = document.createTextNode(batch_id);
+  batch_row.appendChild(text);
+  harvest_row.appendChild(batch_row);
+
+  var strain_row = document.createElement("TD");
+  var text = document.createTextNode(strain);
+  strain_row.appendChild(text);
+  harvest_row.appendChild(strain_row);
+
+  var weight_row = document.createElement("TD");
+  var text = document.createTextNode(weight);
+  weight_row.appendChild(text);
+  harvest_row.appendChild(weight_row);
+
+  body.appendChild(harvest_row);
+
+
   document.getElementById('harvest_greenhouse_form').reset();
-  document.forms['harvest_greenhouse_form'].elements['greenhouse'].value = greenhouse;
-
-
 }
 
 function submit_batches() {
 
   // READ https://stackoverflow.com/questions/10955017/sending-json-to-php-using-ajax
 
-  $.ajax() = ({
-    type: "POST",
-    dataType: "json",
-    url: "harvest_greenhous.php",
-    data: {harvest_list_data:harvest_list},
-    success: submit_batches(data){
-      alert('Greenhouse harvest added');
-  },
-  error: submit_batches(e){
-      console.log(e.message);
-  }
-  });
 }
