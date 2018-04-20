839f8dde3fa80e7de4faa3337ff7aa036de7ff55
Parker Brown <pbrown@acromedia.com>
2018-04-20 11:42:21 -0700

    Cleanup on harvest and added AM / PM harvest option.

--- pages/harvest_script.js
+++ pages/harvest_script.js
@@ -18,7 +18,8 @@ function submit_harvests() {
 
   delete harvest_list;
   delete harvest;
-  window.location.replace('harvest_greenhouse_form.php');
+
+  window.location.replace('home.php');
 }
 
 /**
@@ -31,34 +32,16 @@ function submit_harvests() {
 function add_species_entry_to_list() {
 
   var entry_by = 'species';
-
   document.getElementById("harvest_species_form").addEventListener("click", function(event){
     event.preventDefault()
   });
 
-  // Collect fields and push them onto the harvest_list for greenhouse.
-  var greenhouse = document.getElementsByName('greenhouse')[0].value;
-  var strain = document.getElementsByName('strain')[0].value;
-  var weight = document.getElementsByName('weight')[0].value;
-  var date = document.getElementsByName('date')[0].value;
-  var time = document.getElementsByName('time')[0].value;
-  var notes = document.getElementsByName('notes')[0].value;
-
-
-  var harvest = {};
-  harvest.strain = strain;
-  harvest.weight = weight;
-  harvest.date = date;
-  harvest.time = time;
-  harvest.notes = notes;
-  harvest.greenhouse = greenhouse;
-
+  var harvest = get_harvest_object();
   var validated = is_harvest_valid(harvest);
 
   if (validated) {
 
     harvest_list.push(harvest);
-
     add_harvest_to_queue_table(harvest, entry_by);
 
     if (!lock) {
@@ -72,6 +55,41 @@ function add_species_entry_to_list() {
     document.getElementById('harvest_species_form').reset();
   }
 }
+
+/**
+ * Locks the time of day field so that only one strain is being harvested
+ * Pulls the harvest form data
+ * Validates it
+ * Pushes it onto the harvest list
+ * Populates a table that shows harvests in queue
+ */
+function add_time_entry_to_list() {
+
+  var entry_by = 'time';
+
+  document.getElementById("harvest_time_form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+  var harvest = get_harvest_object();
+  var validated = is_harvest_valid(harvest);
+
+  if (validated) {
+
+    harvest_list.push(harvest);
+    add_harvest_to_queue_table(harvest, entry_by);
+
+    if (!lock) {
+      lock = true;
+      var tableTitle = document.createTextNode("HARVEST FOR \u00A0" + time.toUpperCase());
+      document.getElementsByClassName('table-title')[0].appendChild(tableTitle);
+      document.getElementsByClassName('table-title')[0].style.display = "block";
+    }
+
+    document.getElementById('harvest_time_form').reset();
+  }
+}
+
 /**
  * Locks the Greenhouse field so that only one greenhouse is harvest.
  * Pulls the harvest form data
@@ -87,29 +105,12 @@ function add_greenhouse_entry_to_list() {
     event.preventDefault()
   });
 
-  // Collect fields and push them onto the harvest_list for greenhouse.
-  var greenhouse = document.getElementsByName('greenhouse')[0].value;
-  var strain = document.getElementsByName('strain')[0].value;
-  var weight = document.getElementsByName('weight')[0].value;
-  var date = document.getElementsByName('date')[0].value;
-  var time = document.getElementsByName('time')[0].value;
-  var notes = document.getElementsByName('notes')[0].value;
-
-
-  var harvest = {};
-  harvest.strain = strain;
-  harvest.weight = weight;
-  harvest.date = date;
-  harvest.time = time;
-  harvest.notes = notes;
-  harvest.greenhouse = greenhouse;
-
+  var harvest = get_harvest_object();
   var validated = is_harvest_valid(harvest);
 
   if (validated) {
 
     harvest_list.push(harvest);
-
     add_harvest_to_queue_table(harvest, entry_by);
 
     if (!lock) {
@@ -187,6 +188,18 @@ function add_harvest_to_queue_table(harvest, entry_by) {
     strain_row.appendChild(text);
     harvest_row.appendChild(strain_row);
   }
+  else if (entry_by === 'time') {
+    var greenhouse_row = document.createElement("TD");
+    text = document.createTextNode(harvest.greenhouse);
+    greenhouse_row.appendChild(text);
+    harvest_row.appendChild(greenhouse_row);
+
+    var strain_row = document.createElement("TD");
+    text = document.createTextNode(harvest.strain);
+    strain_row.appendChild(text);
+    harvest_row.appendChild(strain_row);
+  }
+
   var weight_row = document.createElement("TD");
   text = document.createTextNode(harvest.weight);
   weight_row.appendChild(text);
@@ -199,4 +212,28 @@ function add_harvest_to_queue_table(harvest, entry_by) {
 
   body.appendChild(harvest_row);
 
+}
+
+/**
+ * Collects all harvest data and builds a harvest object
+ */
+function  get_harvest_object() {
+
+  var greenhouse = document.getElementsByName('greenhouse')[0].value;
+  var strain = document.getElementsByName('strain')[0].value;
+  var weight = document.getElementsByName('weight')[0].value;
+  var date = document.getElementsByName('date')[0].value;
+  var time = document.getElementById('am').checked ? 'am' : 'pm';
+  var notes = document.getElementsByName('notes')[0].value;
+
+
+  var harvest = {};
+  harvest.strain = strain;
+  harvest.weight = weight;
+  harvest.date = date;
+  harvest.time = time;
+  harvest.notes = notes;
+  harvest.greenhouse = greenhouse;
+
+  return harvest;
 }
