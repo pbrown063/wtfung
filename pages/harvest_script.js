ba6a3c69313bc5b63e4a2e3915e2fd320d783336
Parker Brown <pbrown@acromedia.com>
2018-04-20 10:59:15 -0700

    Added harvest by species and harvest by greenhouse suboptions.

---
+++ pages/harvest_script.js
@@ -0,0 +1,202 @@
+var harvest_list = [];
+var lock = false;
+
+/**
+ * Builds json string and sends it to the server side.
+ * deletes harvest data and refreshes page.
+ */
+function submit_harvests() {
+
+  var json_harvest = JSON.stringify(harvest_list);
+
+  $.ajax({
+    type: "POST",
+    url: "harvest.php",
+    data: {harvest:json_harvest},
+    cache: false,
+  });
+
+  delete harvest_list;
+  delete harvest;
+  window.location.replace('harvest_greenhouse_form.php');
+}
+
+/**
+ * Locks the Strain field so that only one strain is being harvested
+ * Pulls the harvest form data
+ * Validates it
+ * Pushes it onto the harvest list
+ * Populates a table that shows harvests in queue
+ */
+function add_species_entry_to_list() {
+
+  var entry_by = 'species';
+
+  document.getElementById("harvest_species_form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+  // Collect fields and push them onto the harvest_list for greenhouse.
+  var greenhouse = document.getElementsByName('greenhouse')[0].value;
+  var strain = document.getElementsByName('strain')[0].value;
+  var weight = document.getElementsByName('weight')[0].value;
+  var date = document.getElementsByName('date')[0].value;
+  var time = document.getElementsByName('time')[0].value;
+  var notes = document.getElementsByName('notes')[0].value;
+
+
+  var harvest = {};
+  harvest.strain = strain;
+  harvest.weight = weight;
+  harvest.date = date;
+  harvest.time = time;
+  harvest.notes = notes;
+  harvest.greenhouse = greenhouse;
+
+  var validated = is_harvest_valid(harvest);
+
+  if (validated) {
+
+    harvest_list.push(harvest);
+
+    add_harvest_to_queue_table(harvest, entry_by);
+
+    if (!lock) {
+      lock = true;
+      document.getElementsByName("strain")[0].disabled = true;
+      var tableTitle = document.createTextNode("HARVEST FOR \u00A0" + strain.toUpperCase());
+      document.getElementsByClassName('table-title')[0].appendChild(tableTitle);
+      document.getElementsByClassName('table-title')[0].style.display = "block";
+    }
+
+    document.getElementById('harvest_species_form').reset();
+  }
+}
+/**
+ * Locks the Greenhouse field so that only one greenhouse is harvest.
+ * Pulls the harvest form data
+ * Validates it
+ * Pushes it onto the harvest list
+ * Populates a table that shows harvests in queue
+ */
+function add_greenhouse_entry_to_list() {
+
+  var entry_by = 'greenhouse';
+
+  document.getElementById("harvest_greenhouse_form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+  // Collect fields and push them onto the harvest_list for greenhouse.
+  var greenhouse = document.getElementsByName('greenhouse')[0].value;
+  var strain = document.getElementsByName('strain')[0].value;
+  var weight = document.getElementsByName('weight')[0].value;
+  var date = document.getElementsByName('date')[0].value;
+  var time = document.getElementsByName('time')[0].value;
+  var notes = document.getElementsByName('notes')[0].value;
+
+
+  var harvest = {};
+  harvest.strain = strain;
+  harvest.weight = weight;
+  harvest.date = date;
+  harvest.time = time;
+  harvest.notes = notes;
+  harvest.greenhouse = greenhouse;
+
+  var validated = is_harvest_valid(harvest);
+
+  if (validated) {
+
+    harvest_list.push(harvest);
+
+    add_harvest_to_queue_table(harvest, entry_by);
+
+    if (!lock) {
+      lock = true;
+      document.getElementsByName("greenhouse")[0].disabled = true;
+      var tableTitle = document.createTextNode("HARVEST FOR \u00A0" + greenhouse.toUpperCase());
+      document.getElementsByClassName('table-title')[0].appendChild(tableTitle);
+      document.getElementsByClassName('table-title')[0].style.display = "block";
+    }
+
+    document.getElementById('harvest_greenhouse_form').reset();
+  }
+}
+/**
+ * Validates the harvest data.
+ * Only processes one error at a time.
+ */
+function is_harvest_valid(harvest) {
+
+  if (harvest.greenhouse.length > 30) {
+    alert('The greenhouse name is too long.');
+    return false;
+  }
+  else if (harvest.strain.length > 45) {
+    alert('The strain name is too long.');
+    return false;
+  }
+  else if (harvest.time.length > 5) {
+    alert('The time of day is invalid');
+    return false;
+  }
+  else if (harvest.weight <= 0) {
+    alert('Please enter a positive value for the harvest weight.');
+    return false;
+  }
+  else if (isNaN(harvest.weight)) {
+    alert('Please enter a valid number for weight.');
+    return false;
+  }
+  else if (
+      harvest.greenhouse.length < 1 ||
+      harvest.strain.length < 1 ||
+      harvest.time.length < 1 ) {
+    alert('Please fill in all fields.');
+    return false;
+  }
+  else if (harvest.notes.length < 1) {
+    return confirm('Are you sure you want to harvest with no notes?');
+  }
+  return true;
+}
+
+/**
+ * Creates elements and adds a row to the harvest to the harvest queue table.
+ * @param strain
+ * @param weight
+ * @param date
+ */
+function add_harvest_to_queue_table(harvest, entry_by) {
+  document.getElementById("harvest-queue-table").style.display = "block";
+
+  var text = "";
+  var body = document.getElementById("harvest-queue");
+  var harvest_row = document.createElement("TR");
+
+  if (entry_by === 'species') {
+    var greenhouse_row = document.createElement("TD");
+    text = document.createTextNode(harvest.greenhouse);
+    greenhouse_row.appendChild(text);
+    harvest_row.appendChild(greenhouse_row);
+  }
+  else if (entry_by === 'greenhouse') {
+    var strain_row = document.createElement("TD");
+    text = document.createTextNode(harvest.strain);
+    strain_row.appendChild(text);
+    harvest_row.appendChild(strain_row);
+  }
+  var weight_row = document.createElement("TD");
+  text = document.createTextNode(harvest.weight);
+  weight_row.appendChild(text);
+  harvest_row.appendChild(weight_row);
+
+  var date_row = document.createElement("TD");
+  text = document.createTextNode(harvest.date);
+  date_row.appendChild(text);
+  harvest_row.appendChild(date_row);
+
+  body.appendChild(harvest_row);
+
+}
--- pages/harvest_species_form.php
+++ pages/harvest_species_form.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/bootstrap.php';
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Harvest by greenhous</title>
+  <title>Harvest Species</title>
 
   <link rel="stylesheet" type="text/css" href="./CSS/form_style.css">
   <link rel="stylesheet" type="text/css" href="./CSS/main_style.css">
@@ -17,16 +17,16 @@ require_once __DIR__ . '/bootstrap.php';
 <?php include 'header.php'; ?>
 <div class="container">
 
-  <!--Greenhouse field locks upon first harvest entry into queue -->
+  <!--Lock field for strains once harvests are in queue-->
   <ul class="flex-outer">
     <li>
-      <?php display_greehouse();?>
+      <?php display_strains();?>
     </li>
-    <!--    Inner harvest form -->
-    <form method="POST" action="harvest.php" id='harvest_greenhouse_form'>
+    <!--    Inner form that denotes harvest info   -->
+    <form method="POST" action="harvest.php" id='harvest_species_form'>
       <ul class="flex-outer">
         <li>
-          <?php display_strains();?>
+          <?php display_greehouse();?>
         </li>
         <li>
           <input type='number' name='weight' placeholder='Input weight'>
@@ -51,31 +51,32 @@ require_once __DIR__ . '/bootstrap.php';
         </li>
         <li>
 
-
-          <button onclick="return add_greenhouse_entry_to_list()" value='add_harvest'>add harvest</button>
+          <!--        THESE BUTTONS WILL DETERMINE IF MORE HARVESTS WILL BE MADE -->
+          <button onclick="return add_species_entry_to_list()" value='add_harvest'>add harvest</button>
         </li>
       </ul>
     </form>
   </ul>
-</div>
 
-<!--Harvest Queue table-->
+
+</div>
 <h3 class="table-title"> </h3>
 <div class="contain" id="harvest-queue-table">
+
   <table>
     <thead>
     <tr>
-      <th>strain</th>
+      <th>greenhouse</th>
       <th>weight</th>
       <th>date</th>
     </tr>
     </thead>
     <tbody id="harvest-queue">
+
     </tbody>
   </table>
 </div>
 
-<!--Finished harvesting button, sends JSON to server to process-->
 <ul class="flex-outer">
   <li>
     <button form='harvest_form' onclick="submit_harvests()">Finished Harvesting</button>
