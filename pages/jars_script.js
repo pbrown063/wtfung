558a394d4091f312bc11e38ec3c2d69e0852f75e
Parker Brown <pbrown@acromedia.com>
2018-04-21 21:10:29 -0700

    jars form - max plates allowed cannot be greater than number available.

--- pages/jars_script.js
+++ pages/jars_script.js
@@ -1,27 +1,28 @@
 document.getElementById('plate_table').onclick = function(event) {
 
-  var rows = document.querySelectorAll('.plate-row');
-  for ( var i = 0; i < rows.length; i++ ) {
-    if (rows[i].classList.contains("selected-row")) {
-      rows[i].classList.remove("selected-row");
-    }
-  }
-
-
-
   event = event || window.event;
   var target = event.target || event.srcElement;
   while (target && target.nodeName != 'TR') {
     target = target.parentElement;
   }
   var cells = target.cells;
+
+  // Removes selected-row style from all rows and adds it to the target row
+  var rows = document.querySelectorAll('.plate-row');
+  for ( var i = 0; i < rows.length; i++ ) {
+    if (rows[i].classList.contains("selected-row")) {
+      rows[i].classList.remove("selected-row");
+    }
+  }
   cells[0].parentNode.classList.add('selected-row');
+
 
   if (!cells.length || target.parentNode.nodeName == 'THEAD') {
     return;
   }
 
   document.getElementsByName('plate_id')[0].value = cells[0].innerHTML;
+  document.getElementsByName('number_of_plates')[0].setAttribute("max", cells[2].innerHTML);
 
 }
 
